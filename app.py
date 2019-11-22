d3cc30102e0484f9817c5a05e0cf5d6a7aef5d3e
deficts <diegoar7@live.com>
2019-11-21 21:44:26 -0600

    fixing article error

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
-from data import articles
+from data import Articles
+
+Articles = Articles()
 
 app = Flask(__name__)
 
@@ -10,6 +12,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html',articles=Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html',id=id)
 
 if __name__=='__main__':
     app.run(debug=True)
