46456437460da1030d93a322a79664d4f410f416
deficts <diegoar7@live.com>
2019-11-24 12:53:35 -0600

    connection with mysql

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 Articles = Articles()
 
 app = Flask(__name__)
+
+#Config MySQL
+app.config['MYSQL_HOST'] ='localhost'
+app.config['MYSQL_USER'] ='root'
+app.config['MYSQL_PASSWORD'] ='diego.123456'
+app.config['MYSQL_DB'] ='flaskapp'
+app.config['MYSQL_CURSORCLASS'] ='DictCursor'
+
+#init MySQL
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -21,5 +34,43 @@ def articles():
 def article(id):
     return render_template('article.html',id=id)
 
+class RegisterForm(Form):
+    name=StringField('Name',[validators.Length(min=1,max=50)])
+    username=StringField('Username', [validators.Length(min=4,max=25)])
+    email=StringField('Email', [validators.Length(min=6,max=50)])
+    password=PasswordField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirm',message='Passwords do not match')
+    ])
+    confirm=PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form=RegisterForm(request.form)
+    if(request.method=='POST' and form.validate()):
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        #Cursor
+        cur=mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        #Commit
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('You are now registered','success')
+
+        redirect(url_for('index'))
+        return render_template('register.html',form=form)
+    return render_template('register.html',form=form)
+
+
 if __name__=='__main__':
+    app.secret_key='secret123'
     app.run(debug=True)
